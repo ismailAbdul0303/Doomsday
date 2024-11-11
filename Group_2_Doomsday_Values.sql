7355748d4cfca59acfd37e12e27b5faaf1e54bb8
hildaswin <larossman05@gmail.com>
2024-11-11 13:16:03 -0600

    Added table names

--- Group_2_Doomsday_Values.sql
+++ Group_2_Doomsday_Values.sql
@@ -3,3 +3,49 @@
 
 USE [Doomsday]
 GO
+
+-- Power Type IA
+
+-- Power IA
+
+-- Location Type AH
+
+-- Locations AH
+
+-- Water CB 
+
+-- Lodging CB
+
+-- Virus CB 
+
+-- Virus Transmissions CB
+
+-- Virus Transmission Details CB
+
+-- Factions IA
+
+-- Alliances IA
+
+-- Role Types AH
+
+-- Work Roles AH
+
+-- People LR
+
+-- Zombies LR
+
+-- Plants AB
+
+-- Greenhouse AB
+
+-- Item Type LR
+
+-- Items LR
+
+-- Inventory LR
+
+-- Ammo AB
+
+-- Weapon Ammo AB
+
+-- Currency AH
