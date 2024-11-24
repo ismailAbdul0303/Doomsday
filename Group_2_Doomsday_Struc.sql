aa60947c4e918152ea1e7ef20c7c682eefe27297
hildaswin <larossman05@gmail.com>
2024-11-23 19:54:27 -0600

    Added remaining tables
    Created Zombies, ItemType, Items, and Inventory tables

--- Group_2_Doomsday_Struc.sql
+++ Group_2_Doomsday_Struc.sql
@@ -42,10 +42,21 @@ CREATE TABLE People(
     factionKey VARCHAR(8) NULL FOREIGN KEY REFERENCES Factions(factionKey),
     roleKey VARCHAR(8) NULL FOREIGN KEY REFERENCES WorkRoles(roleKey),
 
-    CHECK (Age>=0)
+    CHECK (personAge>=0)
 );
 
 -- Zombies LR
+
+CREATE TABLE Zombies(
+    zomKey VARCHAR(8) PRIMARY KEY NOT NULL,
+    zomFirstName VARCHAR(50) NOT NULL,
+    zomLastName VARCHAR(50) NULL,
+    zomAge INT NULL,
+    factionKey VARCHAR(8) NULL FOREIGN KEY REFERENCES Factions(factionKey),
+    virusKey VARCHAR(8) NULL FOREIGN KEY REFERENCES Virus(virusKey),
+
+    CHECK (zomAge>=0)
+);
 
 -- Plants AB
 
@@ -53,9 +64,29 @@ CREATE TABLE People(
 
 -- Item Type LR
 
+CREATE TABLE ItemType(
+    typeKey VARCHAR(8) PRIMARY KEY NOT NULL,
+    typeName VARCHAR(20) NOT NULL UNIQUE
+);
+
 -- Items LR
 
+CREATE TABLE Items(
+    itemKey VARCHAR(8) PRIMARY KEY NOT NULL,
+    typeKey VARCHAR(8) NOT NULL FOREIGN KEY REFERENCES ItemType(typeKey),
+    itemName VARCHAR(20) UNIQUE NOT NULL,
+    itemInfo TEXT NULL
+);
+
 -- Inventory LR
+
+CREATE TABLE Inventory(
+    factionKey VARCHAR(8) NOT NULL FOREIGN KEY REFERENCES Factions(factionKey),
+    itemKey VARCHAR(8) NOT NULL FOREIGN KEY REFERENCES Items(itemKey),
+    inventoryQty INT NOT NULL DEFAULT 0,
+
+    CHECK (inventoryQty>=0)
+);
 
 -- Ammo AB
 
